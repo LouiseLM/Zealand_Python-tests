2cb704f7eb4a2304a2bbc63726b5e33e6789462b
Bartholomæus <bawa@zealand.dk>
2026-02-03 12:26:56 +0100

    skipping the problematic test cases

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,10 +1,18 @@
 import pytest
 
 def test_pass():
-    # Denne test vil passere
-    assert 1 + 1 == 2
+    #given
+    number1 = 1
+    number2 = 2
+    expectedResult = 3
 
+    #when
+    actualResult = number1+number2
 
+    #then
+    assert expectedResult == actualResult
+
+@pytest.mark.skip(reason="Springes over med vilje") # Denne test bliver slet ikke kørt
 def test_fail():
     # Denne test vil fejle
     assert 1 * 1 == 3
@@ -15,7 +23,7 @@ def test_skip():
     assert False # failed test bliver ignoreret
     raise RuntimeError("Test crashede med vilje") # crash bliver også ignoreret
 
-
+@pytest.mark.skip(reason="Springes over med vilje") # Denne test bliver slet ikke kørt
 def test_crash():
     # Denne test crasher med en exception
     raise RuntimeError("Test crashede med vilje")
